4897ce6ce1cd236b7cd9436753924fcbfc93ecc4
Philipp Achenbach <philipp.achenbach@kom.tu-darmstadt.de>
2019-07-17 09:23:22 +0200

    ...

--- lib/theme/renderer.d.ts
+++
@@ -1,7 +0,0 @@
-import { TPlugin } from 'fela';
-interface ICreateRenderer {
-    plugins?: Array<TPlugin>;
-    css?: string;
-}
-declare const _default: ({ plugins, css }: ICreateRenderer) => import("fela").IRenderer;
-export default _default;
