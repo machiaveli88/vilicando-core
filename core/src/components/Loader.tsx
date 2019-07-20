737f85f2bf63b913add99f454fee5225d87c93c6
Philipp Achenbach <philipp.achenbach@kom.tu-darmstadt.de>
2019-07-20 10:29:59 +0200

    Loader improvements

--- core/src/components/Loader.tsx
+++ core/src/components/Loader.tsx
@@ -1,32 +1,6 @@
 import * as React from 'react';
-import { createRenderer, TRuleProps } from 'fela';
-import { RendererProvider } from 'react-fela';
+import { TRuleProps } from 'fela';
 import { useFela } from '../theme';
-
-const renderer = createRenderer();
-
-const pulseRing = (): TRuleProps => ({
-  '0%': {
-    transform: 'scale(.33)'
-  },
-  '80%, 100%': {
-    opacity: 0
-  }
-});
-renderer.renderKeyframe(pulseRing, {});
-
-const pulseDot = (): TRuleProps => ({
-  '0%': {
-    transform: 'scale(.8)'
-  },
-  '50%': {
-    transform: 'scale(1)'
-  },
-  '100%': {
-    transform: 'scale(.8)'
-  }
-});
-renderer.renderKeyframe(pulseDot, {});
 
 export interface IComponentLoader {
   logo?: string;
@@ -35,94 +9,115 @@ export interface IComponentLoader {
 }
 
 function ComponentLoader({ size = 250, logo, text }: IComponentLoader) {
-  const { css, theme } = useFela();
+  const { css, theme, renderer } = useFela();
+
+  const pulseRing = (): TRuleProps => ({
+    '0%': {
+      transform: 'scale(.33)'
+    },
+    '80%, 100%': {
+      opacity: 0
+    }
+  });
+  renderer.renderKeyframe(pulseRing, {});
+
+  const pulseDot = (): TRuleProps => ({
+    '0%': {
+      transform: 'scale(.8)'
+    },
+    '50%': {
+      transform: 'scale(1)'
+    },
+    '100%': {
+      transform: 'scale(.8)'
+    }
+  });
+  renderer.renderKeyframe(pulseDot, {});
 
   return (
-    <RendererProvider renderer={renderer}>
+    <div
+      className={css({
+        bottom: 0,
+        left: 0,
+        margin: 0,
+        position: 'fixed',
+        pointerEvents: 'none',
+        right: 0,
+        top: 0,
+        background: `linear-gradient(135deg, ${theme.primary7} 0%,${
+          theme.primary9
+        } 100%)`,
+        zIndex: 2000
+      })}
+    >
       <div
         className={css({
-          bottom: 0,
-          left: 0,
-          margin: 0,
-          position: 'fixed',
-          pointerEvents: 'none',
-          right: 0,
-          top: 0,
-          background: `linear-gradient(135deg, ${theme.primary7} 0%,${
-            theme.primary9
-          } 100%)`,
-          zIndex: 2000
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+          height: size * 3,
+          width: size * 3,
+          display: 'block',
+          marginLeft: -size * 1.5,
+          marginTop: -size * 1.5,
+          borderRadius: size * 1.5,
+          backgroundColor: theme.primary7,
+          animation: `k1 1.25s ${theme.easeOut} infinite`
         })}
-      >
+      />
+      {!!logo && (
         <div
           className={css({
             position: 'absolute',
             left: '50%',
             top: '50%',
-            height: size * 3,
-            width: size * 3,
+            height: size,
+            width: size,
             display: 'block',
-            marginLeft: -size * 1.5,
-            marginTop: -size * 1.5,
-            borderRadius: size * 1.5,
-            backgroundColor: theme.primary7,
-            animation: `k1 1.25s ${theme.easeOut} infinite`
+            marginLeft: -size * 0.5,
+            marginTop: -size * 0.5,
+            backgroundColor: theme.white,
+            borderRadius: size * 0.5,
+            boxShadow: theme.shadow2,
+            animation: `k2 1.25s ${theme.easeInOut} -.4s infinite`
           })}
-        />
-        {!!logo && (
+        >
           <div
             className={css({
               position: 'absolute',
               left: '50%',
               top: '50%',
-              height: size,
-              width: size,
+              transform: 'translateX(-50%) translateY(-50%)',
               display: 'block',
-              marginLeft: -size * 0.5,
-              marginTop: -size * 0.5,
-              backgroundColor: theme.white,
-              borderRadius: size * 0.5,
-              boxShadow: theme.shadow2,
-              animation: `k2 1.25s ${theme.easeInOut} -.4s infinite`
+              '> img': {
+                width: size,
+                height: size
+              }
             })}
           >
-            <div
-              className={css({
-                position: 'absolute',
-                left: '50%',
-                top: '50%',
-                transform: 'translateX(-50%) translateY(-50%)',
-                display: 'block',
-                '> img': {
-                  width: size,
-                  height: size
-                }
-              })}
-            >
-              <img src={logo} className="App-logo" alt="logo" />
-            </div>
+            <img src={logo} className="App-logo" alt="logo" />
           </div>
-        )}
-        {!!text && (
-          <div
-            className={css({
-              position: 'absolute',
-              left: 0,
-              bottom: 0,
-              width: '100%',
-              textAlign: 'center',
-              display: 'block',
-              color: 'white',
-              fontFamily: theme.fontFamily,
-              fontSize: theme.fontSizeLg,
-              padding: theme.paddingLg
-            })}
-          >
-            {text}
-          </div>
-        )}
-      </div>
-    </RendererProvider>
+        </div>
+      )}
+      {!!text && (
+        <div
+          className={css({
+            position: 'absolute',
+            left: 0,
+            bottom: 0,
+            width: '100%',
+            textAlign: 'center',
+            display: 'block',
+            color: 'white',
+            fontFamily: theme.fontFamily,
+            fontSize: theme.fontSizeLg,
+            padding: theme.paddingLg
+          })}
+        >
+          {text}
+        </div>
+      )}
+    </div>
   );
 }
 
--- core/src/next/Container.tsx
+++ core/src/next/Container.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import CoreProvider, { ICoreProvider } from '../CoreProvider';
 import { Container as NextContainer } from 'next/app';
-import { IRenderer } from 'fela';
 
 function Container(props: ICoreProvider) {
   return (
