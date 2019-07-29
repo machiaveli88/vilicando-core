e2dfa82d141482b28fe453238406e85d13d506b1
Philipp Achenbach <philipp.achenbach@kom.tu-darmstadt.de>
2019-07-29 20:23:34 +0200

    fela helper

--- core/src/theme/create-renderer.ts
+++ core/src/theme/create-renderer.ts
@@ -6,6 +6,8 @@ import prefixer from 'fela-plugin-prefixer';
 import unit from 'fela-plugin-unit';
 import validator from 'fela-plugin-validator';
 import webPreset from 'fela-preset-web';
+import customProperty from 'fela-plugin-custom-property';
+import friendlyPseudoClass from 'fela-plugin-friendly-pseudo-class';
 import { createRenderer, TPlugin } from 'fela';
 
 interface ICreateRenderer {
@@ -37,6 +39,88 @@ export default ({ plugins = [], css = '' }: ICreateRenderer) => {
         ifSmall: '@media (max-width: 767px, min-width: 480)',
         ifMini: '@media (max-width: 479px)'
       }),
+      customProperty({
+        size: size => ({
+          width: size,
+          height: size
+        }),
+        paddingX: (padding: number | string) => ({
+          paddingLeft: padding,
+          paddingRight: padding
+        }),
+        paddingY: (padding: number | string) => ({
+          paddingTop: padding,
+          paddingBottom: padding
+        }),
+        marginX: (margin: number | string) => ({
+          marginLeft: margin,
+          marginRight: margin
+        }),
+        marginY: (margin: number | string) => ({
+          marginTop: margin,
+          marginBottom: margin
+        }),
+        borderX: (border: string) => ({
+          borderLeft: border,
+          borderRight: border
+        }),
+        borderY: (border: string) => ({
+          borderTop: border,
+          borderBottom: border
+        }),
+        ellipsis: (ellipsis: boolean) =>
+          ellipsis === true
+            ? {
+                whiteSpace: 'nowrap',
+                overflowX: 'hidden',
+                textOverflow: 'ellipsis',
+                maxWidth: '100%'
+              }
+            : {},
+        clearfix: (clearfix: boolean) =>
+          clearfix === true
+            ? {
+                ':after': {
+                  content: '""',
+                  clear: 'both',
+                  display: 'block',
+                  visibility: 'hidden',
+                  height: 0
+                }
+              }
+            : {},
+        center: (center: boolean) =>
+          center === true
+            ? {
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)'
+              }
+            : {},
+        centerX: (center: boolean) =>
+          center === true
+            ? {
+                position: 'absolute',
+                left: '50%',
+                transform: 'translateX(-50%)'
+              }
+            : {},
+        centerY: (center: boolean) =>
+          center === true
+            ? {
+                position: 'absolute',
+                top: '50%',
+                transform: 'translateY(-50%)'
+              }
+            : {},
+        flexWidth: (width: number | string) => ({
+          maxWidth: width,
+          minWidth: width,
+          width
+        })
+      }),
+      friendlyPseudoClass(),
       validator(),
       ...webPreset,
       ...plugins
