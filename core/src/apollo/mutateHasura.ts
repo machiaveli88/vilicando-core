8ccb36a756bab9032a9d81f360481d8c920bc285
Philipp Achenbach <philipp.achenbach@kom.tu-darmstadt.de>
2019-10-26 00:02:32 +0200

    backup mutateHasura

--- core/src/apollo/mutateHasura.ts
+++ core/src/apollo/mutateHasura.ts
@@ -5,50 +5,56 @@ import {
   ExecutionResult,
   MutationResult
 } from '@apollo/react-common';
-import { DocumentNode } from 'graphql';
+import { DocumentNode, VariableDefinitionNode, DefinitionNode } from 'graphql';
 import uuid from 'uuid/v4';
-import { TQueryData } from './queryHasura';
-
-type TData<TItem> = {
-  [x: string]: { __typename: string; returning: Array<TItem> };
-};
+import { TQueryData, TMutationData } from './types';
 
 export default function mutateHasura<
-  TItem extends { id: any },
-  TVariables = OperationVariables
+  TItem extends { id?: any },
+  TVariables = OperationVariables,
+  TQueryVariables = OperationVariables
 >(
   document: DocumentNode,
   query: DocumentNode,
-  options?: MutationHookOptions<TData<TItem>, TVariables>
+  _options?: MutationHookOptions<TMutationData<TItem>, TVariables> & {
+    queryVariables?: TQueryVariables;
+  }
 ): [
   (
-    item: TItem,
-    options?: MutationFunctionOptions<TData<TItem>, TVariables>
-  ) => Promise<ExecutionResult<TData<TItem>>>,
-  MutationResult<TData<TItem>>
+    item: TVariables & Partial<TItem>,
+    options?: MutationFunctionOptions<TMutationData<TItem>, TVariables>
+  ) => Promise<ExecutionResult<TMutationData<TItem>>>,
+  MutationResult<TMutationData<TItem>>
 ] {
-  const [update, result] = useMutation<TData<TItem>, TVariables>(
+  const { queryVariables, ...options } = _options || {};
+  const [update, result] = useMutation<TMutationData<TItem>, TVariables>(
     document,
     options
   );
   const _document = JSON.parse(JSON.stringify(document));
   const definition = _document.definitions.find(
-    ({ kind }: { kind: string }) => kind === 'OperationDefinition'
+    ({ kind }: DefinitionNode) => kind === 'OperationDefinition'
   );
   const { selections } = definition.selectionSet;
-  const __name = selections[0].name.value; // update_user
-  const __type = __name.split('_')[1]; // user
-  const __typename = __type + '_mutation_response'; // user_mutation_response
+  const __name = selections[0].name.value; // z.B. update_user
+  const __type = __name.split('_')[1]; // z.B. user
+  const __typename = __type + '_mutation_response'; // z.B. user_mutation_response
 
   if (selections.length > 1)
     console.warn(
-      "hasura.delete won't work correctly with more than one mutation, please use useMutation instead!"
+      "hasura.mutate won't work correctly with more than one mutation, please use useMutation instead!"
     );
+
+  // update: Ganzes Item kein Problem
+  // delete: Ganzes Item kein Problem
+  // insert: Nur Vars --- das Problem ist, dass id, created-at und updated-at erst mit dem Anlegen erzeugt werden, aber derzeit required sind (zumindest die id) => id müsste unrequired sein und wird ja dann hier erzeugt, falls nicht vorhanden!
+  // updateAll: Ganze Items kein Problem --- das Problem bei xyAll ist, dass man aus dem übergebenen Item keine optimisticResponse erzeugen kann (update würde gehen indem man die Items aus dem Cache holt und nicht die ganzen Einträge austauscht sondern nur die betroffenen Felder).
+  //                      => man könnte überlegen ob man in dem Fall dann nicht ein Item sondern ein Item-Array übergibt, dann könnte es klappen!
 
   return [
     (
-      _item: TItem,
-      options?: MutationFunctionOptions<TData<TItem>, TVariables>
+      _item: TVariables & Partial<TItem>,
+      _options?: MutationFunctionOptions<TMutationData<TItem>, TVariables>
     ) => {
       const item = {
         __typename,
@@ -56,17 +62,44 @@ export default function mutateHasura<
         ..._item
       };
 
-      const variables = {};
-      definition.variableDefinitions.forEach(
-        ({ variable }: { variable: { name: { value: string } } }) => {
-          const key = variable.name.value;
+      let { variables, ...options } = _options || {};
+      if (!variables) {
+        let warn = false;
+        const _variables = {};
+        definition.variableDefinitions.forEach(
+          ({ variable, type }: VariableDefinitionNode) => {
+            const key = variable.name.value;
+            const _type = 'name' in type && type.name.value;
 
-          variables[key] = item[key];
-        }
-      );
+            // todo: werden diese Abfragen noch benötigt, wenn _item: TVariables & Partial<TItem> ???
+            //      dadurch wird ja automatisch garantiert, dass sich die vars und das item mappt
+            //      => dann sollte auch "if (!variables) {" überflüssig werden, da die vars immer automatisch gemappt werden sollten
+            if (
+              !(
+                item[key] !== 'undefined' && // checken ob alle vars !== undefined sind
+                (type.kind !== 'NonNullType' || !!item[key]) && // checken ob alle non-nullable vars !== null sind
+                (!_type || // checken ob alle bei allen vars die typen stimmen
+                  (_type === 'String' && typeof item[key] === 'string') ||
+                  (_type === 'Int' && typeof item[key] === 'number') ||
+                  (_type === 'Float' && typeof item[key] === 'number') ||
+                  (_type === 'Boolean' && typeof item[key] === 'boolean'))
+              )
+            )
+              warn = warn || true;
+
+            _variables[key] = item[key];
+          }
+        );
+
+        if (!warn) variables = _variables as TVariables;
+        else
+          console.warn(
+            'Variables cannot be created automatically from item! Please set manually!'
+          );
+      }
 
       return update({
-        variables: variables as TVariables,
+        variables,
         optimisticResponse: (vars: {} | TVariables) => ({
           [__name]: {
             __typename,
@@ -81,19 +114,32 @@ export default function mutateHasura<
             }
           }
         ) => {
-          const cacheData = cache.readQuery<TQueryData<TItem>>({
-            query
-            // variables todo: Möglichkeit der query noch variablen mitgeben zu können
-          });
+          const cacheData = cache.readQuery<TQueryData<TItem>, TQueryVariables>(
+            {
+              query,
+              variables: queryVariables
+            }
+          );
+
           const data = {
             ...cacheData,
-            [__type]: [
-              ...cacheData[__type].filter(({ id }) => id !== item.id),
-              ...returning.map(item => ({ __optimistic: false, ...item }))
-            ]
+            [__type]: [...cacheData[__type]]
           };
 
-          cache.writeQuery({
+          returning.forEach(item => {
+            const index = cacheData[__type].findIndex(
+              ({ id }) => id === item.id
+            );
+
+            if (~index) data[__type][index] = item;
+            else data[__type].push(item);
+          });
+          data[__type] = data[__type].map((item: TQueryData<TItem>) => ({
+            __optimistic: false,
+            ...item
+          }));
+
+          cache.writeQuery<TQueryData<TItem>, TQueryVariables>({
             query,
             data
           });
