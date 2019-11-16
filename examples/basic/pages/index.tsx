aada851674a8e638697258d6a63e03a69ad6b345
Philipp Achenbach <philipp.achenbach@kom.tu-darmstadt.de>
2019-11-16 18:31:41 +0100

    env-example

--- examples/basic/pages/index.tsx
+++ examples/basic/pages/index.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 
 function StartPage() {
-  console.log('App runs on port ' + process.env.PORT);
-
   return (
     <>
       <h1>Hello World</h1>
---
+++ examples/env/pages/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+function StartPage() {
+  return (
+    <p>In this example, port {process.env.PORT} is set using the .env-file.</p>
+  );
+}
+
+export default StartPage;
