4f553222e1ab0ebf0b2ee2dff03ba9cd6a998650
Philipp Achenbach <philipp.achenbach@kom.tu-darmstadt.de>
2019-11-16 17:01:56 +0100

    fix dynamic import

--- core/src/next/dynamic.tsx
+++ core/src/next/dynamic.tsx
@@ -1,31 +1,45 @@
 import * as React from 'react';
 import dynamic, { DynamicOptions, Loader } from 'next/dynamic';
 
+interface IDynamicOptions<P = {}> extends DynamicOptions<P> {
+  onSSR?: ({
+    error,
+    isLoading,
+    pastDelay
+  }: {
+    error?: Error | null;
+    isLoading?: boolean;
+    pastDelay?: boolean;
+    timedOut?: boolean;
+  }) => JSX.Element | null;
+}
+
 function dynamicWithSSR<P = {}>(
-  dynamicOptions: DynamicOptions<P> | Loader<P>,
-  options?: DynamicOptions<P> & {
-    onSSR?: ({
-      error,
-      isLoading,
-      pastDelay
-    }: {
-      error?: Error | null;
-      isLoading?: boolean;
-      pastDelay?: boolean;
-      timedOut?: boolean;
-    }) => JSX.Element | null;
-  }
+  dynamicOptions: IDynamicOptions<P> | any, // todo: Loader<P> instead of any leads to error: Der Typ "Readonly<{}>" kann dem Typ "never" nicht zugewiesen werden.
+  options?: IDynamicOptions<P>
 ): React.ComponentType<P> {
+  if (dynamicOptions instanceof Promise) {
+    options.loader = () => dynamicOptions;
+    // Support for having import as a function, eg: dynamic(() => import('../hello-world'))
+  } else if (typeof dynamicOptions === 'function') {
+    options.loader = dynamicOptions;
+    // Support for having first argument being options, eg: dynamic({loader: import('../hello-world')})
+  } else if (typeof dynamicOptions === 'object') {
+    options = { ...dynamicOptions };
+  }
+
   const { loading, onSSR, ...rest } = options;
   const isSSR = typeof window === 'undefined';
 
-  return loading || onSSR
-    ? dynamic(dynamicOptions, {
-        loading: props => (isSSR && onSSR ? onSSR : loading)(props),
-        ssr: !onSSR,
-        ...rest
-      })
-    : dynamic(dynamicOptions, options);
+  return dynamic(
+    loading || onSSR
+      ? {
+          loading: props => (isSSR && onSSR ? onSSR : loading)(props),
+          ssr: !onSSR,
+          ...rest
+        }
+      : options
+  );
 }
 
 export default dynamicWithSSR;
