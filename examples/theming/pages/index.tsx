53157ce3c232cca64e62660a40edca977325c94e
Philipp Achenbach <philipp.achenbach@kom.tu-darmstadt.de>
2019-11-30 14:02:36 +0100

    small improvement & antd up

--- examples/theming/pages/index.tsx
+++ examples/theming/pages/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { useFela } from 'vilicando-core';
 
 function StartPage() {
+  const { css, theme } = useFela();
+
   return (
     <>
       <h1>Hello World</h1>
       <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet.
+        <span className={css({ color: theme.redColor, fontWeight: 'bold' })}>
+          Lorem ipsum dolor sit amet
+        </span>
+        , consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+        accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+        sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor
+        sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+        vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+        gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
       </p>
     </>
   );
