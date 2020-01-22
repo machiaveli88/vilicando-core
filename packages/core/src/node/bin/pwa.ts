0babe7975c48d80a51fad3d612e39716bd27deac
Philipp Achenbach <philipp.achenbach@kom.tu-darmstadt.de>
2020-01-22 11:35:30 +0100

    pwa uses config-vars

--- packages/core/src/node/bin/pwa.ts
+++ packages/core/src/node/bin/pwa.ts
@@ -2,7 +2,6 @@
 import { join } from 'path';
 import { generateImages } from 'pwa-asset-generator';
 import { existsSync, readFileSync, writeFileSync, readdirSync } from 'fs';
-import { ITheme } from '../../theme';
 
 interface IManifest {
   name?: string;
@@ -48,53 +47,56 @@ interface IManifest {
 
 const publicDir = join(process.cwd(), 'public');
 const manifestPath = join(publicDir, 'manifest.json');
-const themePath = join(process.cwd(), 'theme.json');
+const configPath = join(process.cwd(), 'config.json');
 
 export const pwa = async () => {
-  const logoName =
-    existsSync(publicDir) &&
-    readdirSync(publicDir).find(name => name.split('.')?.[0] === 'logo');
+  const config =
+    existsSync(configPath) &&
+    (JSON.parse(readFileSync(configPath, 'utf8')) || {});
 
-  if (logoName) {
-    const logoPath = join(publicDir, logoName);
+  if (config) {
+    const { name, shortName, description, logo, theme } = config;
+    const logoName =
+      (existsSync(join(publicDir, logo)) && logo) || // logo-name/path is given in config...
+      (existsSync(publicDir) &&
+        readdirSync(publicDir).find(name => name.split('.')?.[0] === 'logo')); // ...or search for logo in public-dir
 
-    // theme laden
-    let theme: ITheme = {};
-    if (existsSync(themePath))
-      theme = JSON.parse(readFileSync(themePath, 'utf8')) || {};
+    if (logoName) {
+      const logoPath = join(publicDir, logoName);
 
-    // manifest generieren
-    let data: IManifest = {};
-    if (existsSync(manifestPath))
-      data = JSON.parse(readFileSync(manifestPath, 'utf8')) || {};
-    data = {
-      ...data,
-      // name,
-      // short_name,
-      display: data.display || 'standalone',
-      background_color: data.background_color || theme.primary.base,
-      theme_color: data.background_color || theme.primary.base
-    };
-    writeFileSync(manifestPath, JSON.stringify(data), 'utf8');
-    console.info('  ✔ manifest.json created!');
+      // manifest generieren
+      let data: IManifest = {};
+      if (existsSync(manifestPath))
+        data = JSON.parse(readFileSync(manifestPath, 'utf8')) || {};
+      data = {
+        ...data,
+        name: data.name || name,
+        short_name: data.short_name || shortName,
+        description: data.description || description,
+        display: data.display || 'standalone',
+        background_color: data.background_color || theme.primary.base,
+        theme_color: data.background_color || theme.primary.base
+      };
+      writeFileSync(manifestPath, JSON.stringify(data), 'utf8');
+      console.info('  ✔ manifest.json created!');
 
-    // pwas generieren
-    const { htmlMeta } = await generateImages(
-      logoPath,
-      join(publicDir, 'images'),
-      {
-        background: data.background_color,
-        opaque: false,
-        scrape: false,
-        manifest: manifestPath,
-        favicon: true,
-        log: false
-      }
-    );
+      // pwas generieren
+      /* const { htmlMeta } = */ await generateImages(
+        logoPath,
+        join(publicDir, 'images'),
+        {
+          background: data.background_color,
+          opaque: false,
+          scrape: false,
+          manifest: manifestPath,
+          favicon: true,
+          log: false
+        }
+      );
 
-    // todo: safari-pinned-tab.svg generieren
-    // nur strokes, diese schwarz, 16x16 Pixel!
-    /* if (logoName.split('.').reverse()?.[0] === 'svg') {
+      // todo: safari-pinned-tab.svg generieren
+      // nur strokes, diese schwarz, 16x16 Pixel!
+      /* if (logoName.split('.').reverse()?.[0] === 'svg') {
       writeFileSync(
         join(publicDir, 'images/safari-pinned-tab.svg'),
         readFileSync(logoPath, 'utf8').replace(
@@ -109,13 +111,14 @@ export const pwa = async () => {
         '  ✘ logo has to be .svg, to create automatically safari-pinned-tab.svg!'
       ); */
 
-    // todo:
-    // htmlMeta in Head schreiben!
-    // logoName variabel machen
-    // background variabel machen
-    // name/short-name variabel machen
-    // Idee: Skript wird automatisch aufgerufen, schaut ob ein Logo vorhanden ist, wenn ja schaut es ob alle icons + manifest vorhanden sind, wenn nein wird Skript ausgeführt (ansonsten nur wenn explizit aufgerufen!!!)!
+      // todo:
+      // htmlMeta in Head schreiben!
+      // logoName variabel machen
+      // background variabel machen
+      // name/short-name variabel machen
+      // Idee: Skript wird automatisch aufgerufen, schaut ob ein Logo vorhanden ist, wenn ja schaut es ob alle icons + manifest vorhanden sind, wenn nein wird Skript ausgeführt (ansonsten nur wenn explizit aufgerufen!!!)!
 
-    console.info('  ✔ pwa-assets generated!');
-  } else console.info('  ✘ no logo found!');
+      console.info('  ✔ pwa-assets generated!');
+    } else console.info('  ✘ no logo found!');
+  } else console.info('  ✘ no config.json found!');
 };
