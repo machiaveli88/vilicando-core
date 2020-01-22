a463bd890f65115c4aa0b4dba9a1df2f6c57de17
Philipp Achenbach <philipp.achenbach@kom.tu-darmstadt.de>
2020-01-22 12:55:30 +0100

    pwa-example improvements

--- examples/pwa/pages/_app.tsx
+++ examples/pwa/pages/_app.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { App, AppProvider } from 'vilicando-core';
 import { Layout } from '@components';
+import config from '../config.json';
 
 export default class CustomApp extends App {
   render() {
     const { Component, pageProps } = this.props;
 
     return (
-      <AppProvider name="PWA Example App" isPWA>
+      <AppProvider {...config}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
