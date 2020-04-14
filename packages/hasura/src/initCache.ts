47e6e95ab446c559c8d5e180755043c5af4b903f
Philipp Achenbach <philipp.achenbach@kom.tu-darmstadt.de>
2020-04-14 22:22:02 +0200

    update

--- packages/hasura/src/initCache.ts
+++ packages/hasura/src/initCache.ts
@@ -5,14 +5,14 @@ import { PersistentStorage, PersistedData } from "apollo-cache-persist/types";
 export default function initCache(state: NormalizedCacheObject = {}) {
   const cache = new InMemoryCache().restore(state);
 
-  if (typeof window !== "undefined") {
+  // todo: https://github.com/apollographql/apollo-cache-persist#how-do-i-wait-for-the-cache-to-be-restored-before-rendering-my-app
+  if (typeof window !== "undefined")
     persistCache({
       cache,
       storage: window.localStorage as PersistentStorage<
         PersistedData<NormalizedCacheObject>
       >,
     });
-  }
 
   return cache;
 }
